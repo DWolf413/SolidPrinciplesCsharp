48d0ffa958a376a99b57f4a4b631e32e9471398c
David Logacho Torres <davidlogacho13@outlook.com>
2023-02-19 18:03:18 -0500

    Base to refactoring Liskov principle

---
+++ 3-LiskovSubstitution/LiskovSubstitution/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public class Employee
+    {
+        public string Fullname { get; set; }
+        public int HoursWorked { get; set; }
+        public int ExtraHours { get; set; }
+
+        public Employee(string fullname, int hoursWorked)
+        {
+            Fullname = fullname;
+            HoursWorked = hoursWorked;
+        }
+
+        public virtual decimal CalculateSalary(bool IsFullTime) 
+        {
+            decimal hourValue = IsFullTime ? 50 : 40;
+            return hourValue * (HoursWorked + ExtraHours);
+        }
+    }
+}
