f907f887dc3ada447e369862cef46b1ebb317ba0
David Logacho Torres <davidlogacho13@outlook.com>
2023-02-19 19:33:03 -0500

    Ading code to refactoring

---
+++ 5-DepndecyInversion/Api/DependecyInversion/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using DependecyInversion.Models;
+using DependecyInversion.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependecyInversion.Controllers
+{
+    [ApiController, Route("student")]
+    public class StudentController : ControllerBase
+    {
+        StudentRepository studentRepository = new StudentRepository();
+        Logbook logbook = new Logbook();
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            logbook.Add($"returning student's list");
+            return studentRepository.GetAll();
+        }
+
+        [HttpPost]
+        public void Add([FromBody] Student student)
+        {
+            studentRepository.Add(student);
+            logbook.Add($"The Student {student.Fullname} have been added");
+        }
+    }
+}
---
+++ 5-DepndecyInversion/Api/DependecyInversion/Models/Logbook.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace DependecyInversion.Models
+{
+    public class Logbook
+    {
+        public void Add(string description)
+        {
+            File.AppendAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logbook.txt"), $"{description}\n", Encoding.Unicode);
+        }
+    }
+}
